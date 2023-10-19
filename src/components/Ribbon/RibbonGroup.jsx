37719246cd7138966175c47fd309b0eafced4cba
mqasim23 <qasimdeveloper2023@gmail.com>
2023-10-19 14:59:36 +0100

    new dist file

--- src/components/Ribbon/RibbonGroup.jsx
+++ src/components/Ribbon/RibbonGroup.jsx
@@ -1,6 +1,8 @@
 import { excludeKeys } from '../../utils';
 import { RibbonGroup } from 'react-bootstrap-ribbon';
 import SelectComponent from '../SelectComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap-ribbon/dist/react-bootstrap-ribbon.css';
 
 const CustomRibbonGroup = ({ data }) => {
   const updatedData = excludeKeys(data);
