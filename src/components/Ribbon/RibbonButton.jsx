3a76ddec15c4f4e675e10dd0b9a8e8eda0ef234b
qasim <qasimdeveloper2023@gmail.com>
2023-12-11 17:30:05 +0500

    handle the select event on the ribbon

--- src/components/Ribbon/RibbonButton.jsx
+++ src/components/Ribbon/RibbonButton.jsx
@@ -1,17 +1,43 @@
 import * as Icons from './RibbonIcons';
 import { RibbonButton } from 'react-bootstrap-ribbon';
 import { Row, Col } from 'reactstrap';
+import { useAppData } from '../../hooks';
 
 const CustomRibbonButton = ({ data }) => {
-  const { Icon, Caption } = data?.Properties;
+  const { socket } = useAppData();
+
+  const { Icon, Caption, Event } = data?.Properties;
+
+  const handleSelectEvent = () => {
+    const selectEvent = JSON.stringify({
+      Event: {
+        EventName: 'Select',
+        ID: data?.ID,
+      },
+    });
+    const exists = Event && Event.some((item) => item[0] === 'Select');
+    if (!exists) return;
+    console.log(selectEvent);
+    socket.send(selectEvent);
+  };
+
+  const handleButtonEvent = () => {
+    handleSelectEvent();
+  };
 
   const IconComponent = Icons[Icon];
   return (
     <Row>
       <Col md={12}>
-        <div className='d-flex align-items-center flex-column justify-content-center'>
+        <div
+          className='d-flex align-items-center flex-column justify-content-center'
+          onClick={handleButtonEvent}
+          style={{ cursor: 'pointer' }}
+        >
           <IconComponent size={35} />
-          <div className='text-center' style={{fontSize:'12px'}}>{Caption}</div>
+          <div className='text-center' style={{ fontSize: '12px' }}>
+            {Caption}
+          </div>
         </div>
       </Col>
     </Row>
--- src/components/Ribbon/RibbonButtonGroup.jsx
+++ src/components/Ribbon/RibbonButtonGroup.jsx
@@ -1,20 +1,46 @@
 import * as AppIcons from './RibbonIcons';
 import { Row, Col } from 'reactstrap';
+import { useAppData } from '../../hooks';
 
 const CustomRibbonButtonGroup = ({ data }) => {
+  const { socket } = useAppData();
+
   const dataSplit = (title) => {
     return title.substring(0, 5) + '..';
   };
-  const { Captions, Icons } = data?.Properties;
+  const { Captions, Icons, Event } = data?.Properties;
 
   const colSize = Captions?.length == 4 ? 6 : 12;
+
+  const handleSelectEvent = (info) => {
+    const selectEvent = JSON.stringify({
+      Event: {
+        EventName: 'Select',
+        ID: data?.ID,
+        Info: info,
+      },
+    });
+    const exists = Event && Event.some((item) => item[0] === 'Select');
+    if (!exists) return;
+    console.log(selectEvent);
+    socket.send(selectEvent);
+  };
+
+  const handleButtonEvent = (info) => {
+    handleSelectEvent(info);
+  };
 
   return (
     <Row>
       {Captions.map((title, i) => {
         let IconComponent = AppIcons[Icons[i]];
         return (
-          <Col md={colSize} className='d-flex align-items-center justify-content-center '>
+          <Col
+            md={colSize}
+            className='d-flex align-items-center justify-content-center'
+            style={{ cursor: 'pointer' }}
+            onClick={() => handleButtonEvent(i + 1)}
+          >
             <IconComponent size={25} />
             <div style={{ fontSize: '12px', textAlign: 'center' }}>{title?.slice(0, 10)}</div>
           </Col>
