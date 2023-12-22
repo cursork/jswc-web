b4752086491b9066dee297a4f1e9e2c1682cf03c
qasim <qasimdeveloper2023@gmail.com>
2023-12-22 14:58:17 +0500

    updated the timer

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -6,52 +6,38 @@ const Timer = ({ data }) => {
 
   const { Interval, FireOnce, Active, Event } = data?.Properties;
 
-  console.log({ Active });
+  const [fireOnce, setFireOnce] = useState(data?.Properties.FireOnce);
 
-  const [fire, setFire] = useState(!FireOnce ? 0 : FireOnce);
   useEffect(() => {
-    if (Active === 1 && fire !== 2) {
-      // Fire the timer event
-      const timerEvent = JSON.stringify({
-        Event: { EventName: 'Timer', ID: data?.ID, Info: [] },
-      });
-      socket.send(timerEvent);
-      // If FireOnce is set to 1, update it to 2 to prevent further firing
-      if (FireOnce === 1) {
-        setFire(2);
-        // You might need to update the FireOnce value in your state or data source
-        // to ensure the change is reflected in subsequent renders.
+    let intervalId;
+    const timerEvent = JSON.stringify({
+      Event: { EventName: 'Timer', ID: data?.ID, Info: [] },
+    });
+
+    // check Active is 1
+    if (Active == 1) {
+      if (fireOnce == 1) {
+        intervalId = setInterval(() => {
+          socket.send(timerEvent);
+          setFireOnce(2);
+        }, Interval && Interval);
+        return () => {
+          clearInterval(intervalId);
+        };
+      } else if (fireOnce == 2) {
+        clearInterval(intervalId);
+      } else if (fireOnce == 0) {
+        intervalId = setInterval(() => {
+          socket.send(timerEvent);
+        }, Interval && Interval);
       }
+      return () => {
+        clearInterval(intervalId);
+      };
     }
-
-    // Clear the interval if Active becomes 0 or if FireOnce is 2
-    const intervalId =
-      Active === 1 && fire !== 2
-        ? setInterval(() => {
-            const timerEvent = JSON.stringify({
-              Event: { EventName: 'Timer', ID: data?.ID, Info: [] },
-            });
-            socket.send(timerEvent);
-          }, Interval && Interval[0])
-        : null;
-
-    return () => {
-      // Clear the interval when the component unmounts or when conditions are no longer met
-      clearInterval(intervalId);
-    };
-  }, [Active, FireOnce, Interval]);
+  }, [data]);
 
   return <></>;
 };
 
 export default Timer;
-
-//   useEffect(() => {
-//     const intervalId = setInterval(() => {
-//       const timerEvent = JSON.stringify({
-//         Event: { EventName: 'Timer', ID: data?.ID, Info: [] },
-//       });
-//       socket.send(timerEvent);
-//     }, Interval && Interval[0]);
-//     return () => clearInterval(intervalId);
-//   }, []);
