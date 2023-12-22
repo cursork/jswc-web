c84c09c3fe5d5304488fd839ee6644facad23dab
qasim <qasimdeveloper2023@gmail.com>
2023-12-22 16:46:44 +0500

    updated the subform

--- src/components/SubForm.jsx
+++ src/components/SubForm.jsx
@@ -1,9 +1,23 @@
 import { excludeKeys } from '../utils';
 import SelectComponent from './SelectComponent';
 import TextArea from './TextArea';
+import { useEffect } from 'react';
 
 const SubForm = ({ data }) => {
   const updatedData = excludeKeys(data);
+  const parentSize = JSON.parse(localStorage.getItem('formDimension'));
+
+  const { Size, Posn } = data?.Properties;
+
+  useEffect(() => {
+    localStorage.setItem(
+      data.ID,
+      JSON.stringify({
+        Size: !Size ? [parentSize[0], parentSize[1]] : Size,
+        Posn: !Posn ? [0, 0] : Posn,
+      })
+    );
+  }, []);
 
   return (
     <div style={{ position: 'relative', border: '1px solid #FAFAFA' }}>
