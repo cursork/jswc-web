95ec3322115265eff09e4760b405dccb7fcec560
qasim <qasimdeveloper2023@gmail.com>
2024-01-01 15:46:24 +0500

    added support for dbl click on listView

--- src/components/ListView/index.jsx
+++ src/components/ListView/index.jsx
@@ -4,18 +4,49 @@ import { useAppData } from '../../hooks';
 const ListView = ({ data }) => {
   const PORT = localStorage.getItem('PORT');
 
-  const { findDesiredData } = useAppData();
+  const { findDesiredData, socket } = useAppData();
 
-  const { View, Border, ColTitles, ImageIndex, ImageListObj, Items, ReportInfo } =
+  const { View, Border, ColTitles, ImageIndex, ImageListObj, Items, ReportInfo, Event } =
     data && data?.Properties;
 
   const styles = setStyle(data?.Properties);
 
-  //   console.log({ View });
+  const handleDoubleClickEvent = (index, shiftState) => {
+    const event = JSON.stringify({
+      Event: {
+        EventName: 'ItemDblClick',
+        ID: data?.ID,
+        Info: [index, 1, shiftState, 4],
+      },
+    });
 
-  //   console.log({ data });
+    // Stored in local storage to handle the WG of TreeView
+    const SelItems = new Array(Items?.length).fill(0);
 
-  // Icon bigger Icon
+    SelItems[index - 1] = 1;
+
+    //WG SelItems
+    const storedFocusedIndex = JSON.stringify({
+      Event: {
+        SelItems: SelItems,
+      },
+    });
+    localStorage.setItem(data?.ID, storedFocusedIndex);
+    const exists = Event && Event.some((item) => item[0] === 'ItemDblClick');
+    if (!exists) return;
+    console.log(event);
+    socket.send(event);
+  };
+
+  const handleDoubleClick = (nativeEvent, index) => {
+    const isAltPressed = nativeEvent?.altKey ? 4 : 0;
+    const isCtrlPressed = nativeEvent?.ctrlKey ? 2 : 0;
+    const isShiftPressed = nativeEvent?.shiftKey ? 1 : 0;
+    const mouseButton = nativeEvent?.button;
+    let shiftState = isAltPressed + isCtrlPressed + isShiftPressed;
+
+    handleDoubleClickEvent(index, shiftState);
+  };
 
   const ImageListView = ({
     orientation = 'row',
@@ -34,8 +65,9 @@ const ListView = ({ data }) => {
         {Items?.map((item, index) => {
           return (
             <div
-              style={{ width, height }}
-              className={`d-flex flex-${orientation} align-items-center`}
+              onDoubleClick={(e) => handleDoubleClick(e.nativeEvent, index + 1)}
+              style={{ width, height, cursor: 'pointer' }}
+              className={`d-flex flex-${orientation} align-items-center `}
             >
               <img
                 style={{ width: imageWidth, height: imageHeight }}
@@ -124,19 +156,30 @@ const ListView = ({ data }) => {
 
         <div className='mt-1'>
           {/* Report */}
-          {reportsData?.map((report) => {
+          {reportsData?.map((report, index) => {
             return (
               <div className='d-flex align-items-center'>
-                <div style={{ flex: 1, paddingLeft: '5px' }}>
+                <div
+                  style={{ flex: 1, paddingLeft: '5px', cursor: 'pointer' }}
+                  onDoubleClick={(e) => handleDoubleClick(e.nativeEvent, index)}
+                >
                   <div className='d-flex align-items-center'>
                     <img src={`http://localhost:${PORT}${report?.image}`} />
                     <span style={{ fontSize: '12px' }}>{report?.title}</span>
                   </div>
                 </div>
-                <div style={{ flex: 1, fontSize: '12px', paddingLeft: '5px' }}>
+                <div
+                  style={{ flex: 1, fontSize: '12px', paddingLeft: '5px', cursor: 'pointer' }}
+                  onDoubleClick={(e) => handleDoubleClick(e.nativeEvent, index)}
+                >
                   {report?.description}
                 </div>
-                <div style={{ flex: 1, fontSize: '12px', paddingLeft: '5px' }}>{report?.index}</div>
+                <div
+                  style={{ flex: 1, fontSize: '12px', paddingLeft: '5px', cursor: 'pointer' }}
+                  onDoubleClick={(e) => handleDoubleClick(e.nativeEvent, index)}
+                >
+                  {report?.index}
+                </div>
               </div>
             );
           })}
