3713383712753efcff063b3dee799ae0301e6609
Sarim <sarimhassan5@gmail.com>
2024-02-02 11:26:31 +0500

    updated the WG for the Splitter

--- src/components/Splitter/VerticalSplitter.jsx
+++ src/components/Splitter/VerticalSplitter.jsx
@@ -57,7 +57,7 @@ const VerticalSplitter = ({ data }) => {
         Event: {
           EventName: emitEvent && emitEvent[0],
           ID: data.ID,
-          Info: [0, calculateLeft, formWidth, 3],
+          Info: [0, calculateLeft],
           Size: [formHeight, 3],
         },
       })
