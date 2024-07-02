b60d3714c6a2a6e4c954a71f75a1276d368bf64f
qasim <qasimdeveloper2023@gmail.com>
2024-07-02 11:39:33 +0500

    adding optional border and caption

--- src/components/MessageBox/index.jsx
+++ src/components/MessageBox/index.jsx
@@ -1,41 +1,53 @@
 import React from "react";
 import "./MessageBox.css";
+import { isElement } from "lodash";
 
 const MsgBox = ({ data, onClose, isDesktop }) => {
   console.log("msgbox", data);
   const { Caption, Text, Style, Btns } = data?.Properties;
-  console.log({Caption, Text, Style, Btns, isDesktop})
+  console.log({ Caption, Text, Style, Btns, isDesktop });
   const getIconClass = () => {
     switch (Style) {
-      case 'Info':
-        return 'info-icon';
-      case 'Query':
-        return 'query-icon';
-      case 'Warn':
-        return 'warn-icon';
-      case 'Error':
-        return 'error-icon';
+      case "Info":
+        return "info-icon";
+      case "Query":
+        return "query-icon";
+      case "Warn":
+        return "warn-icon";
+      case "Error":
+        return "error-icon";
       default:
-        return '';
+        return "";
     }
   };
+
+  let renderCheck = !!!isDesktop
   return (
     <div className="msgbox-overlay">
-      <div className="msgbox-container">
-        { !!!isDesktop && <div className="msgbox-header">{Caption}</div> }
+      <div className={`msgbox-container ${renderCheck? 'with-border': ''}`}>
+        {renderCheck && <div className= "msgbox-header with-border">{Caption}</div>}
         <div className="msgbox-body">
-          {Style && Style !== 'Msg' && <span className={`icon ${getIconClass()}`}></span>}
+          {Style && Style !== "Msg" && (
+            <span className={`icon ${getIconClass()}`}></span>
+          )}
           <span>{Text}</span>
         </div>
-        <div className="msgbox-footer">
+        <div className={`msgbox-footer ${renderCheck ? 'with-border': ''}`}>
           {Array.isArray(Btns) ? (
             Btns.map((btn, index) => (
-              <button key={index} className="rounded-button " onClick={() => onClose(`MsgBtn${index + 1}`, data?.ID)}>
+              <button
+                key={index}
+                className="rounded-button "
+                onClick={() => onClose(`MsgBtn${index + 1}`, data?.ID)}
+              >
                 {btn.toLowerCase()}
               </button>
             ))
           ) : (
-            <button className="rounded-button" onClick={() => onClose('MsgBtn1', data?.ID)}>
+            <button
+              className="rounded-button"
+              onClick={() => onClose("MsgBtn1", data?.ID)}
+            >
               {Btns}
             </button>
           )}
