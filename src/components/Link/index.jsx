b57e82d23ff51c70ef62cff3c2dedaf81c8e2429
Neil Kirsopp <git@kirsopp.me>
2024-10-05 16:43:29 +0200

    Basic ability to have links and uploads
    This is functional only and not polished - there are no styles etc.
    Everything in here is also subject to change.

---
+++ src/components/Link/index.jsx
@@ -0,0 +1,16 @@
+const Link = ({data}) => {
+    console.log('LINKDATA', data);
+    return (
+        <div>
+            <a id={data.Properties.ID}
+                href={data.Properties.Href}
+                target={data.Properties?.Target || '_blank'} // Default to new tab/window
+                download={data.Properties?.Download === 1}
+            >
+                {data.Properties?.Label !== undefined ? data.Properties.Label : data.Properties.Href}
+            </a>
+        </div>
+    );
+};
+
+export default Link;
