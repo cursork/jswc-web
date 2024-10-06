62da54514707c1e1bab4dfa51bc9b922a1f3b129
Neil Kirsopp <git@kirsopp.me>
2024-10-06 12:51:22 +0200

    Only return the right properties

--- src/App.jsx
+++ src/App.jsx
@@ -1209,6 +1209,13 @@ const App = () => {
               },
             },
           };
+          const props = resp.WG.Properties;
+          for (const p in props) {
+            if (!serverEvent.Properties.includes(p)) {
+              delete props[p];
+            }
+          }
+          
           if (file) {
             if (serverEvent.Properties.includes('FileBytes')) {
               const reader = new FileReader();
